68c48388b722fa05223f5591fb1fa683af47bdf1
saltyaom <saltyaom@gmail.com>
2022-11-10 20:19:31 +0700

    :broom: chore: update kingworld and hono

--- benchmarks/bun/kingworld/index.ts
+++ benchmarks/bun/kingworld/index.ts
@@ -2,15 +2,13 @@ import KingWorld from 'kingworld'
 
 const app = new KingWorld()
 
-const xPoweredBy = 'benchmark'
-
 app.get('/', () => 'Hi')
-   .post('/json', (ctx) => ctx.body)
-   .get('/id/:id', (ctx) => {
-       ctx.responseHeaders.set('x-powered-by', xPoweredBy)
+    .post('/json', (ctx) => ctx.body)
+    .get('/id/:id', (ctx) => {
+        ctx.set.headers['x-powered-by'] = 'benchmark'
 
-       return `${ctx.params.id} ${ctx.query.name}`
-   })
-   .listen(3000)
+        return `${ctx.params.id} ${ctx.query.name}`
+    })
+    .listen(3000)
 
 export default app
