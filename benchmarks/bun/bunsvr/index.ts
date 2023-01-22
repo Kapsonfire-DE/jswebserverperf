705d7e8ca372219ce52a5da2c39c0b5fb9114da1
Dennis Dudek <dudek@adesso-mobile.de>
2023-01-22 15:48:14 +0100

    update bunsvr

--- benchmarks/bun/bunsvr/index.ts
+++ benchmarks/bun/bunsvr/index.ts
@@ -7,12 +7,12 @@ router.static("GET", "/",
 );
 
 router.static("POST", "/json",
-              async req => new Response(JSON.stringify(await req.json()))
+              async req => Response.json(await req.json())
 );
 
 router.dynamic("GET", "/id/:id",
                async (req, server, params) => {
-                   const name = new URL(req.url).searchParams.get("name");
+                   const name = req.url.slice(params[0].length + 6);
 
                    return new Response(`${params[1]} ${name}`, {
                        headers: { "x-powered-by": "benchmark" }
